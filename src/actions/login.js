f8e750b7b9b92b5699c587690abce12682c0a46d
Joe Sak <joe@joesak.com>
2016-02-27 15:06:13 -0500

    Keep login/logout actions separated in their own files

---
+++ src/actions/login.js
@@ -0,0 +1,58 @@
+export const LOGIN_REQUEST = 'LOGIN_REQUEST'
+export const LOGIN_SUCCESS = 'LOGIN_SUCCESS'
+export const LOGIN_FAILURE = 'LOGIN_FAILURE'
+
+const ROOT_URL = 'http://localhost:3001'
+
+function requestLogin(creds) {
+  return {
+    type: LOGIN_REQUEST,
+    isFetching: true,
+    isAuthenticated: false,
+    creds
+  }
+}
+
+function receiveLogin(user) {
+  return {
+    type: LOGIN_SUCCESS,
+    isFetching: false,
+    isAuthenticated: true,
+    id_token: user.id_token
+  }
+}
+
+function loginError(message) {
+  return {
+    type: LOGIN_FAILURE,
+    isFetching: false,
+    isAuthenticated: false,
+    message
+  }
+}
+
+export function loginUser(creds) {
+  let config = {
+    method: 'POST',
+    headers: { 'Content-Type':'application/x-www-form-urlencoded' },
+    body: `username=${creds.username}&password=${creds.password}`
+  }
+
+  return dispatch => {
+    dispatch(requestLogin(creds))
+
+    return fetch(`${ROOT_URL}/sessions/create`, config)
+      .then(response =>
+        response.json().then(user => ({ user, response }))
+      ).then(({ user, response }) =>  {
+        if (!response.ok) {
+          dispatch(loginError(user.message))
+          return Promise.reject(user)
+        } else {
+          localStorage.setItem('id_token', user.id_token)
+          dispatch(receiveLogin(user))
+        }
+      }
+    ).catch(err => console.log("Error: ", err))
+  }
+}
